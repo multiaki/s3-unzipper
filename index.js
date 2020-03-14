457393b28f07164752e81374e2b308c8eacd646c
Aki Sharipov <aki@multiaki.com>
2020-03-14 02:19:10 -0400

    Delete extra files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const filePath = "newfile.csv";
 const bucketName = "aws-zipfolder";
 const key = "newfile.csv.zip";
 
+AWS.config.update({ region: "us-east-1" });
 let s3 = new AWS.S3();
 
 let index = 0;
@@ -29,7 +30,18 @@ const downloadFile = async (filePath, bucketName, key) => {
       console.log("end");
     });
 
-  return await streamToS3(body)
+  let result =  await streamToS3(body)
+
+  let message = {
+    Message: JSON.stringify({ Bucket: bucketName, Key: filePath }),
+    TopicArn: "<YOUR-TOPIC-ARN>"
+  };
+
+  var publishTextPromise = new AWS.SNS({ apiVersion: "2010-03-31" })
+    .publish(message)
+    .promise();
+
+  return publishTextPromise;
 };
 
 downloadFile(filePath, bucketName, key).then(
